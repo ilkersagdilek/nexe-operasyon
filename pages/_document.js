994c3075bb030dfb737bda0eb295acef645497aa
ilkersagdilek <98395793+ilkersagdilek@users.noreply.github.com>
2026-07-15 12:30:19 +0200

    Remove icon asset dependency

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,6 @@ export default function Document() {
   return (
     <Html lang="tr">
       <Head>
-        <link rel="icon" href="/icon.png" />
-        <link rel="apple-touch-icon" href="/icon.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#1B2C48" />
         <meta name="mobile-web-app-capable" content="yes" />
