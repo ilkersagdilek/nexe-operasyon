4ae370423171cf522d5e1f884a998e4e1d385d53
ilkersagdilek <98395793+ilkersagdilek@users.noreply.github.com>
2026-07-15 12:59:51 +0200

    Use app icon as favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="tr">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
         <meta name="theme-color" content="#1B2C48" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
